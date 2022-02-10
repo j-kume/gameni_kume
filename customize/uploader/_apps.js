7565be2f83ee347118e26f7ed875084edb1f1fc6
kume <j-kume@aisic.jp>
2022-02-10 22:46:17 +0900

    アプリ指定でのcustomize-uploadの制御ができるように修正

--- customize/uploader/_apps.js
+++ customize/uploader/_apps.js
@@ -3,6 +3,10 @@ export const apps_all = {
     { app: 0, code: "template", name: "テンプレート" }
   ],
   dev: [
-    { app: 999, code: "template", name: "【開発】テンプレート" }
+    { app: 1, code: "customer", name: "顧客リスト" },
+    { app: 2, code: "project", name: "案件管理" },
+    { app: 3, code: "daily_report", name: "日報" },
+    { app: 4, code: "files_management", name: "ファイル管理" },
+    { app: 5, code: "contract_management", name: "契約書管理" }
   ]
 };

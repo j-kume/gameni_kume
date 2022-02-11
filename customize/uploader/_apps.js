524535880cde2022035c1c2de3f32154c21c4574
kume <j-kume@aisic.jp>
2022-02-12 01:30:50 +0900

    アプリ名の修正

--- customize/uploader/_apps.js
+++ customize/uploader/_apps.js
@@ -3,10 +3,10 @@ export const apps_all = {
     { app: 0, code: "template", name: "テンプレート" }
   ],
   dev: [
-    { app: 1, code: "customer", name: "顧客リスト" },
-    { app: 2, code: "project", name: "案件管理" },
-    { app: 3, code: "daily_report", name: "日報" },
-    { app: 4, code: "files_management", name: "ファイル管理" },
-    { app: 5, code: "contract_management", name: "契約書管理" }
+    { app: 1, code: "customer", name: "【開発】顧客リスト" },
+    { app: 2, code: "project", name: "【開発】案件管理" },
+    { app: 3, code: "daily_report", name: "【開発】日報" },
+    { app: 4, code: "files_management", name: "【開発】ファイル管理" },
+    { app: 5, code: "contract_management", name: "【開発】契約書管理" }
   ]
 };

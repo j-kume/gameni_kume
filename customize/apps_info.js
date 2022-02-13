3b3256dcc72e6065182d70b5fc519e2627406aa5
kume <j-kume@aisic.jp>
2022-02-13 15:03:40 +0900

    アプリ情報を別ファイル化

---
+++ customize/apps_info.js
@@ -0,0 +1,82 @@
+//=============================================================================
+//【ファイル名】
+//    apps.ts
+//【アプリ名】
+//    設定情報
+//【タイプ】
+//    ○PC用 / ○スマートフォン用 / ●共通用
+//-----------------------------------------------------------------------------
+//  Copyright (c) 2022 AISIC.Inc
+//=============================================================================
+export const configs = [
+  {
+    env: 'dev',
+    code: 'number_ctrl',
+    name: '【開発】番号管理マスタ',
+    id: 10,
+    token: 'BYs5rfJqgOS7jKGj7yHmG4Ob9hN4HMjy4aRlbima'
+  },
+  {
+    env: 'dev',
+    code: 'error_log',
+    name: '【開発】エラーログ',
+    id: 7,
+    token: 'Ic5TjV6WbhMluVfFAgG5eYbL5HfoevVJOcpy6j3X'
+  },
+  {
+    env: 'dev',
+    code: 'customer',
+    name: '【開発】顧客リスト',
+    id: 1,
+    token: 'Lu2XevvEFAWRfSKOaaI7hmZcacQv0THXVh8PJx3H'
+  },
+  {
+    env: 'dev',
+    code: 'project',
+    name: '【開発】案件管理',
+    id: 2,
+    token: 'Xayrvxc8Kv805ZY6g2yETvssN4l7w3k7s1OxW64Q'
+  },
+  {
+    env: 'dev',
+    code: 'daily_report',
+    name: '【開発】日報',
+    id: 3,
+    token: 'w7fapwYLq10jL67TiulAV7TMzhpjBowgMFwOeMf8'
+  },
+  {
+    env: 'dev',
+    code: 'files_management',
+    name: '【開発】ファイル管理',
+    id: 4,
+    token: 'y2tHpOU6Gao11R6OlqzkhmVpHEHw8gLkCO50xS5u'
+  },
+  {
+    env: 'dev',
+    code: 'contract_management',
+    name: '【開発】契約書管理',
+    id: 5,
+    token: 'r7QsN5Fjc4jREwJSsvoLaLyZv5sxHRnY0amj2gpc'
+  },
+  {
+    env: 'pro',
+    code: 'number_ctrl',
+    name: '番号管理マスタ',
+    id: 0,
+    token: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  },
+  {
+    env: 'pro',
+    code: 'error_log',
+    name: 'エラーログ',
+    id: 0,
+    token: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  },
+  {
+    env: 'pro',
+    code: 'customer',
+    name: '顧客リスト',
+    id: 0,
+    token: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  },
+];
--- customize/src/common/apps.ts
+++ customize/src/common/apps.ts
@@ -8,6 +8,9 @@
 //-----------------------------------------------------------------------------
 //  Copyright (c) 2022 AISIC.Inc
 //=============================================================================
+import * as apps_info  from "../../apps_info";
+// require('apps_')
+
 
 export class Apps {
 
@@ -15,57 +18,7 @@ export class Apps {
   private _env: string;
 
   constructor(id: number) {
-    this._confs = [
-      {
-        env: 'dev',
-        code: 'number_ctrl',
-        name: '【開発】番号管理マスタ',
-        id: 10,
-        token: 'BYs5rfJqgOS7jKGj7yHmG4Ob9hN4HMjy4aRlbima'
-      },
-      {
-        env: 'dev',
-        code: 'error_log',
-        name: '【開発】エラーログ',
-        id: 7,
-        token: 'Ic5TjV6WbhMluVfFAgG5eYbL5HfoevVJOcpy6j3X'
-      },
-      {
-        env: 'dev',
-        code: 'customer',
-        name: '【開発】顧客リスト',
-        id: 1,
-        token: 'Lu2XevvEFAWRfSKOaaI7hmZcacQv0THXVh8PJx3H'
-      },
-      {
-        env: 'dev',
-        code: 'project',
-        name: '【開発】案件管理',
-        id: 2,
-        token: 'Xayrvxc8Kv805ZY6g2yETvssN4l7w3k7s1OxW64Q'
-      },
-      {
-        env: 'dev',
-        code: 'daily_report',
-        name: '【開発】日報',
-        id: 3,
-        token: 'w7fapwYLq10jL67TiulAV7TMzhpjBowgMFwOeMf8'
-      },
-      {
-        env: 'dev',
-        code: 'files_management',
-        name: '【開発】ファイル管理',
-        id: 4,
-        token: 'y2tHpOU6Gao11R6OlqzkhmVpHEHw8gLkCO50xS5u'
-      },
-      {
-        env: 'dev',
-        code: 'contract_management',
-        name: '【開発】契約書管理',
-        id: 5,
-        token: 'r7QsN5Fjc4jREwJSsvoLaLyZv5sxHRnY0amj2gpc	'
-      }
-    ];
+    this._confs = apps_info.configs;
 
     this._env = this.getEnvById(id);
 
